99c4ea9c375187c86b4b55cd0aa3d343a7c84951
Fernando <kinggprobr@gmail.com>
2023-08-12 12:56:52 -0300

    improving code

--- qrcode-generator.py
+++ qrcode-generator.py
@@ -1,18 +1,22 @@
 import qrcode
 from qrcode.image.styledpil import StyledPilImage
+from qrcode.image.styles.colormasks import SolidFillColorMask
 
-value = input(str("Insira o conteúdo para gerar um QR Code: "))
+value = input(str("Insert a link or text: "))
 
 qr = qrcode.QRCode(
-    version=1,
+    version=8,
     error_correction=qrcode.constants.ERROR_CORRECT_H,
     box_size=20,
     border=1,
 )
 qr.add_data(value)
 
-img = qr.make_image(image_factory=StyledPilImage,embeded_image_path=".\img\pyy-logo.png")
+img = qr.make_image(
+    image_factory=StyledPilImage,
+    embeded_image_path=".\img\logo.png",
+    color_mask=SolidFillColorMask(back_color=(255,255,255), front_color=(19, 17, 15))
+)
 
 img.save(".\img\qrcode.png")
-
-print("Imagem gerada com sucesso!")
+print(f"\nQR Code successfully generated!\n")
